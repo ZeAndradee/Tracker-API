7ca38aefe1302624088136c8f1a4206179c1f03a
Vinicius <vinciuscarmo95@gmail.com>
2024-07-10 16:34:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import userRoutes from "./routes/user.js";
 import spotifyInfoRoutes from "./routes/spotifydata.js";
 import logsRoutes from "./routes/logs.js";
+import tracksRoutes from "./routes/tracks.js";
 import cors from "cors";
 
 const app = express();
@@ -12,5 +13,6 @@ app.use(cors());
 app.use("/", userRoutes);
 app.use("/", spotifyInfoRoutes);
 app.use("/", logsRoutes);
+app.use("/", tracksRoutes);
 
 app.listen(3000);

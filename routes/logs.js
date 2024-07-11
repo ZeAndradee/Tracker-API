e8fdf38933787904fd4266dcc9c40085e72da47b
Vinicius <59659214+ZeAndradee@users.noreply.github.com>
2024-07-11 18:06:38 +0000

    changes routes

--- routes/logs.js
+++ routes/logs.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post("/logs", addLog);
 
-router.get("/logs/", getLog);
+router.get("/logs", getLog);
 router.get("/logs/:trackurl", getLog);
 
 router.put("/logs/:id", updateLog);

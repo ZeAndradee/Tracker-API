f49d7415546efd2a45816212555e3ac68c734386
Vinicius <vinciuscarmo95@gmail.com>
2024-07-12 16:13:43 -0300

    trying to make connection D O

--- config.js
+++ config.js
@@ -1,7 +1,28 @@
 import { config as dotenvConfig } from "dotenv";
 import mysql from "mysql2";
+import fs from "fs";
+import path from "path";
 
 dotenvConfig();
 
-const connection = mysql.createConnection(process.env.MYSQL_URL);
+const connectionConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+  ssl: {
+    ca: fs.readFileSync(path.resolve(__dirname, "certs/ca-certificate.crt")),
+  },
+};
+
+const connection = mysql.createConnection(connectionConfig);
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Erro ao conectar ao banco de dados:", err);
+    return;
+  }
+  console.log("Conectado ao banco de dados!");
+});
 export default connection;
